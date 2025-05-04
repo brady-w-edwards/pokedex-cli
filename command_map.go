f02aea4e5c8cc3183f6c4fe32713cdf8c3773954
Brady Edwards <bwe731@gmail.com>
2025-05-04 15:56:07 -0500

    created pokecache package for creating a cache object

--- command_map.go
+++ command_map.go
@@ -6,34 +6,38 @@ import (
 	"io"
 	"net/http"
 	"pokedex-cli/internal/config"
+	"pokedex-cli/internal/pokecache"
 )
 
-func commandMap(c *config.Preview) error {
+func commandMap(c *config.Preview, cache *pokecache.Cache) error {
 	url := "https://pokeapi.co/api/v2/location-area"
 
 	if c.Next != nil {
 		url = *c.Next
 	}
+	body, found := cache.Get(url)
 
-	res, err := http.Get(url)
-	if err != nil {
-		return fmt.Errorf("request failed: %s", err)
-	}
+	if !found {
+		res, err := http.Get(url)
+		if err != nil {
+			return fmt.Errorf("request failed: %s", err)
+		}
 
-	body, err := io.ReadAll(res.Body)
-	defer res.Body.Close()
+		body, err := io.ReadAll(res.Body)
+		defer res.Body.Close()
 
-	if res.StatusCode > 299 {
-		return fmt.Errorf("response failed with status code: %d", res.StatusCode)
-	}
-	if err != nil {
-		return fmt.Errorf("failed to read body: %s", err)
-	}
+		if res.StatusCode > 299 {
+			return fmt.Errorf("response failed with status code: %d", res.StatusCode)
+		}
+		if err != nil {
+			return fmt.Errorf("failed to read body: %s", err)
+		}
 
-	var locations config.LocationsArea
-	err = json.Unmarshal(body, &locations)
-	if err != nil {
-		return fmt.Errorf("error unmarshalling data: %s", err)
+		var locations config.LocationsArea
+		err = json.Unmarshal(body, &locations)
+		if err != nil {
+			return fmt.Errorf("error unmarshalling data: %s", err)
+		}
 	}
 
 	c.Next = locations.Next
--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -6,9 +6,8 @@ import (
 )
 
 type Cache struct {
-	mu       sync.Mutex
-	entries  map[string]cacheEntry
-	interval time.Duration
+	mu      *sync.Mutex
+	entries map[string]cacheEntry
 }
 
 type cacheEntry struct {
@@ -16,27 +15,44 @@ type cacheEntry struct {
 	val       []byte
 }
 
-func NewCache(interval time.Duration) *Cache {
-	c := new(Cache)
-	c.reapLoop()
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
+		entries: make(map[string]cacheEntry),
+		mu:      &sync.Mutex{},
+	}
+	go c.reapLoop(interval)
 	return c
 }
 
 func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.entries[key] = cacheEntry{createdAt: time.Now(), val: val}
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
-	if c.entries[key].val != nil {
-		return c.entries[key].val, true
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, exists := c.entries[key]
+	if !exists {
+		return nil, false
 	}
-	return nil, false
+	return entry.val, true
 }
 
-func (c *Cache) reapLoop() {
-	for _, entry := range c.entries {
-		if time.Since(entry.createdAt) > c.interval {
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
 
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for k, v := range c.entries {
+		if v.createdAt.Before(now.Add(-last)) {
+			delete(c.entries, k)
 		}
 	}
 }
